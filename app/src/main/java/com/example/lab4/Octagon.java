ab39fd82c10d4a5bddc56a3f8b30f61a33bc6f63
Robbie Feng <robbie020428@gmail.com>
2023-10-05 21:35:00 -0600

    Add color to Octagon, solves the conflict

--- app/src/main/java/com/example/lab4/Octagon.java
+++ app/src/main/java/com/example/lab4/Octagon.java
@@ -3,5 +3,7 @@ package com.example.lab4;
 public class Octagon extends Shape {
     public Octagon() {
         x = 1;
+        color = "cyan";//This solves the conflict :)
     }
+
 }
--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -4,4 +4,5 @@ public abstract class Shape {
     protected Integer x, y;
 
     protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+
 }

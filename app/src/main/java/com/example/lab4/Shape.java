aa23ff82af4c14ab1f5f9bc2d5b1d3e7efbc354f
Shikhararora19 <shikhar3@ualberta.ca>
2023-10-05 23:11:34 -0600

    Update Shape.java

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,6 +3,6 @@ package com.example.lab4;
 public abstract class Shape {
     protected Integer x, y;
 
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "blue";
 
 }

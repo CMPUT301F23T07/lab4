94d17bd72cce3b391e951fb2945529410e2440a7
Youngwoo Choi <cyan246010@gmail.com>
2023-10-10 14:34:44 -0600

    Update Shape.java

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -2,6 +2,5 @@ package com.example.lab4;
 
 public abstract class Shape {
     protected Integer x, y;
-
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "blue";
 }
